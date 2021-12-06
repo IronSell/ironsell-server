39d1e8f345535e3c10d3d48d8959d55d89f20bee
nachbr91 <nabero91@gmail.com>
2021-12-06 22:05:43 +0100

    Edit company model

--- models/Company.model.js
+++ models/Company.model.js
@@ -17,6 +17,7 @@ const companySchema = new Schema(
       type: String,
       maxlength: 9,
       required: [true, 'CIF is required'],
+      unique: true,
     },
     address: {
       type: String,
@@ -30,7 +31,7 @@ const companySchema = new Schema(
       type: [Schema.Types.ObjectId],
       ref: 'JobOffer',
     },
-    companyUrl: [{type: Schema.Types.Url}],
+    companyUrl: [{ type: Schema.Types.Url }],
   },
   {
     timestamps: true,
