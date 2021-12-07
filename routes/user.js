7ceb0fbc8f8f038d706bc2fedf093e758594b1ea
Rox <you@example.com>
2021-12-07 19:30:02 +0100

    edit user routes

--- routes/user.js
+++ routes/user.js
@@ -1,2 +1,24 @@
 const router = require("express").Router();
+
+//Models
 const User = require("../models/User.model");
+const Session = require('../models/Session.model');
+
+//Middleware
+const isLoggedIn = require('../middleware/isLoggedIn');
+const isLoggedOut = require('../middleware/isLoggedOut');
+
+//Get user profile
+router.get('/user/:_id', isLoggedIn, async (req, res) => {
+    try {
+      const showUser = await User.find(req.params._id);
+      // res.status(200).json({ message:  })
+    } catch (err) {
+      return res.status(404).json({ errorMessage: 'User profile does not exist'})
+    }
+  })
+  
+  module.exports = router;
+//Get favorites
+//Get applied offers
+//Get edit profile
