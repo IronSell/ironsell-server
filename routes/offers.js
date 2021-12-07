fd9645798181dc55b6a4a9f421b0d00391f07a8a
Rox <you@example.com>
2021-12-08 00:36:01 +0100

    edit offers routes

---
+++ routes/offers.js
@@ -0,0 +1,42 @@
+const router = require("express").Router();
+
+//Models
+const JobOffer = require("../model/JobOffer.model")
+
+//Middleware
+const isLoggedIn = require('../middleware/isLoggedIn');
+const isLoggedOut = require('../middleware/isLoggedOut');
+
+
+
+//GET search offers
+router.get('/', async (req, res) => {
+    try {
+      const searchOffers = await JobOffer.find();
+      return res.status(200).json({ message: 'Offers found', searchOffers});
+    } catch (err) {
+      return res.status(404).json({ errorMessage: 'Offers not found'})
+    }
+  })
+
+//GET offer
+router.get('/:_id', isLoggedIn, async (req, res) => {
+    try {
+      const showOffer = await JobOffer.find(req.params._id);
+      return res.status(200).json({ message: 'Offer found', showOffer });
+    } catch (err) {
+      return res.status(404).json({ errorMessage: 'Offer not found'})
+    }
+  })
+
+//DELETE offer
+router.delete('/delete/:_id', isLoggedIn, async (req, res) => {
+    try {
+      const deletedOffer = await JobOffer.find(req.params._id);
+      return res.status(200).json({ message: 'Offer deleted', deletedOffer });
+    } catch (err) {
+      return res.status(404).json({ errorMessage: 'Cannot delete the offer'})
+    }
+  })
+
+  module.exports = router;
