56b55481363dcce4f9a62b1330ffc921194a663c
nachbr91 <nabero91@gmail.com>
2021-12-08 00:35:09 +0100

    Edit candidates, companies and users routes files

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const authRoutes = require('./auth');
-const companyRoutes = require('./company');
+const companyRoutes = require('./companies');
+const candidatesRoutes = require('./candidates');
+const usersRoutes = require('./users')
 
 /* GET home page */
 router.get('/', (req, res, next) => {
@@ -8,6 +10,8 @@ router.get('/', (req, res, next) => {
 });
 
 router.use('/auth', authRoutes);
-router.use('/company', companyRoutes);
+router.use('/companies', companyRoutes);
+router.use('/candidates', candidatesRoutes);
+router.use('/users', usersRoutes);
 
 module.exports = router;
---
+++ routes/users.js
@@ -0,0 +1,57 @@
+const router = require('express').Router();
+
+//Models
+const User = require('../models/User.model');
+const Company = require('../models/Company.model');
+const JobOffer = require('../models/JobOffer.model');
+
+//Middleware
+const isLoggedIn = require('../middleware/isLoggedIn');
+const isLoggedOut = require('../middleware/isLoggedOut');
+
+//Get user profile
+router.get('/profile', isLoggedIn, async (req, res) => {
+  try {
+    return res.status(200).json({ message: 'Profile found', showUser });
+  } catch (err) {
+    return res.status(404).json({ errorMessage: 'This user does not' });
+  }
+});
+
+//Get search offers
+router.get('/offers', async (req, res) => {
+  try {
+    const searchOffers = await JobOffer.find();
+    return res.status(200).json({ message: 'Offers found', searchOffers });
+  } catch (err) {
+    return res.status(404).json({ errorMessage: 'Offers not found' });
+  }
+});
+
+//Get offer
+router.get('/offers/:_id', isLoggedIn, async (req, res) => {
+  try {
+    const showOffer = await JobOffer.find(req.params._id);
+    return res.status(200).json({ message: 'Offer found', showOffer });
+  } catch (err) {
+    return res.status(404).json({ errorMessage: 'Offer not found' });
+  }
+});
+
+//Get favorites offers
+
+
+//Get applied offers
+
+
+//UPDATE/EDIT profile
+router.patch('/edit/:_id', isLoggedIn, async (req, res) => {
+  try {
+    const editProfile = await User.findByIdAndUpdate(req.params._id);
+    return res.status(200).json({ message: 'Profile edited', editProfile });
+  } catch (err) {
+    return res.status(400).json({ message: 'Cannot update the profile' });
+  }
+});
+
+module.exports = router;
