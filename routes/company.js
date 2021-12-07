78ece2a1db0c85e8c09a29dd3657a17ace0480a7
nachbr91 <nabero91@gmail.com>
2021-12-07 12:07:04 +0100

    Edit company.js route file

--- routes/company.js
+++ routes/company.js
@@ -8,4 +8,12 @@ const Session = require('../models/Session.model');
 const isLoggedIn = require('../middleware/isLoggedIn');
 const isLoggedOut = require('../middleware/isLoggedOut');
 
-//
+// GET company page
+router.get('/company/:_id', isLoggedIn, async (req, res) => {
+  try {
+    const showCompany = await Company.find(req.params._id);
+    // res.status(200).json({ message:  })
+  } catch (err) {
+    return res.status(404).json({ errorMessage: 'Company page does not exist'})
+  }
+})
