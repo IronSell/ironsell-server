ddca448c378aae373013c761093a295dbb7527a7
nachbr91 <nabero91@gmail.com>
2021-12-07 19:29:12 +0100

    Edit company route

--- routes/company.js
+++ routes/company.js
@@ -3,12 +3,13 @@ const router = require('express').Router();
 // Models
 const Company = require('../models/Company.model');
 
+
 //Middleware
 const isLoggedIn = require('../middleware/isLoggedIn');
 const isLoggedOut = require('../middleware/isLoggedOut');
 
 // GET company page
-router.get('/:_id', isLoggedIn, async (req, res) => {
+router.get('/:_id', async (req, res) => {
   try {
     const showCompany = await Company.find(req.params._id);
     return res.status(200).json({ message: 'Company found', showCompany });
@@ -18,5 +19,12 @@ router.get('/:_id', isLoggedIn, async (req, res) => {
       .json({ errorMessage: 'Company page does not exist' });
   }
 });
+
+router.patch('/update/:_id', isLoggedIn, async (req, res) => {
+  try {
+    const editCompnay = await Company.findByIdAndUpdate(req.params._id)
+    return res.status(200).json({ message: 'Company edited', editCompany })
+  } catch
+})
 
 module.exports = router;
