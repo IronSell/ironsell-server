c0c89883ac267744400c622ffaf36795e4f30572
Federico Altava Redondo <fedealtava@gmail.com>
2021-12-07 19:35:19 +0100

    edit user rout

--- routes/user.js
+++ routes/user.js
@@ -1,2 +1,5 @@
 const router = require("express").Router();
 const User = require("../models/User.model");
+
+const isLoggedIn = require('../middleware/isLoggedIn');
+const isLoggedOut = require('../middleware/isLoggedOut');
