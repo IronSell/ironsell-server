00ba0aaaac585476f0ccd80c9920af28962db484
Rox <you@example.com>
2021-12-08 08:56:58 +0100

    add offersRoutes to index.js

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const authRoutes = require('./auth');
 const companyRoutes = require('./companies');
 const candidatesRoutes = require('./candidates');
 const usersRoutes = require('./users')
+const offersRoutes = require('/offers')
 
 /* GET home page */
 router.get('/', (req, res, next) => {
@@ -13,5 +14,6 @@ router.use('/auth', authRoutes);
 router.use('/companies', companyRoutes);
 router.use('/candidates', candidatesRoutes);
 router.use('/users', usersRoutes);
+router.use('/offers', offersRoutes)
 
 module.exports = router;
