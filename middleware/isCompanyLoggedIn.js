78676c277d00d7cd5cb332d4441f3d773a916774
nachbr91 <nabero91@gmail.com>
2021-12-21 00:50:52 +0100

    Edit middleware

--- middleware/isCompanyLoggedIn.js
+++ middleware/isCompanyLoggedIn.js
@@ -1,18 +1,17 @@
-const SessionCompany = require("../models/SessionCompany.model");
+const SessionCompany = require('../models/SessionCompany.model');
 
 module.exports = (req, res, next) => {
-  // checks if the user is logged in when trying to access a specific page
-  if (!req.headers.authorization || req.headers.authorization === "null") {
-    return res.status(403).json({ errorMessage: "You are not logged in" });
+  if (!req.headers.authorization || req.headers.authorization === 'null') {
+    return res.status(403).json({ errorMessage: 'You are not logged in' });
   }
 
   SessionCompany.findById(req.headers.authorization)
-    .populate({ path: "user", model: "Company" })
+    .populate({ path: 'user', model: 'Company' })
     .then((session) => {
       if (!session) {
         return res
           .status(404)
-          .json({ errorMessage: "No session started for this user" });
+          .json({ errorMessage: 'No session started for this user' });
       }
       // makes the user available in `req.user` from now onwards
       req.user = session.user;
